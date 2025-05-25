29e774bc3b4e2c3c97c0b6047208b651904d5c23
younghyun753 <younghyun75398@gmail.com>
2025-05-25 16:49:33 +0900

    feat: token req, token res 인터페이스 추가

--- src/types.ts
+++ src/types.ts
@@ -45,3 +45,12 @@ export interface IssueInfo {
     total_count: number;
   };
 } 
+
+export interface TokenResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface TokenRequest {
+  githubCode: string;
+}
